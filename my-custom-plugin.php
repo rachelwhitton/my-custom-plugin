13d67f8f37c1a698c30f0b20f6b2390dbeeda795
Rachel Whitton <rachel@getpantheon.com>
2017-10-01 13:51:18 -0500

    Initial commit

---
+++ my-custom-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     My Custom Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     my-custom-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         My_Custom_Plugin
+ */
